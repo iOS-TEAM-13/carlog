ed2f819fba3aac1deb831c94450996049f539254
Luna828 <eskw0701@naver.com>
2023-10-11 12:58:11 +0900

    [FEAT]: Model, Views, Constants 파일 swift 파일 생성 -> clone시 Error 방지

---
+++ CarLog/CarLog/Models/Model.swift
@@ -0,0 +1,3 @@
+import Foundation
+
+//clone 시, Error 뜨는 것 방지를 위한 파일
---
+++ CarLog/CarLog/Views/TableViewCell.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+//clone 시, Error 뜨는 것 방지를 위한 파일
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
