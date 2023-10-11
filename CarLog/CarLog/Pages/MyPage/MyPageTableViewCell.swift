114bb3a760909f87f364aea2884ebc7e89ecb4f3
p2glet <vviian.2@gmail.com>
2023-10-11 22:59:07 +0900

    [FEAT] : created TableView

---
+++ CarLog/CarLog/Pages/MyPage/MyPageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageTableViewCell.swift
+//  CarLog
+//
+//  Created by t2023-m0056 on 2023/10/11.
+//
+
+import UIKit
+
+class MyPageTableViewCell: UITableViewCell {
+    static var identifier = "MyPageTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
