74d7320c59489a2b5c71f9afdeda9fb5d3524a34
Luna828 <eskw0701@naver.com>
2023-10-11 10:17:42 +0900

    [FEAT]: Font "Jua" 설정하기

--- CarLog/CarLog/Controllers/MyCarPageViewController.swift
+++ CarLog/CarLog/Controllers/MyCarPageViewController.swift
@@ -1,12 +1,22 @@
 import UIKit
 
 class MyCarPageViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor.white
+
+        //MARK - 예시
+        view.addSubview(timeTitle)
+        timeTitle.translatesAutoresizingMaskIntoConstraints = false
+        timeTitle.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        timeTitle.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 
+    lazy var timeTitle: UILabel = {
+        let label = UILabel()
+        label.text = "시간"
+        label.font = UIFont(name: "Jua", size: 16)
 
+        return label
+    }()
 }
-
