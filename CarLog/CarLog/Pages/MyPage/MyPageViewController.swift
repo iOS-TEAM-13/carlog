b0ef4b040070abc3898d69084553d547851b225d
p2glet <vviian.2@gmail.com>
2023-10-12 01:10:29 +0900

    [FEAT] : created tableVIew in MyCarPage

--- CarLog/CarLog/Pages/MyPage/MyPageViewController.swift
+++ CarLog/CarLog/Pages/MyPage/MyPageViewController.swift
@@ -1,54 +1,10 @@
 import UIKit
-import SnapKit
-import SwiftUI
 
 class MyPageViewController: UIViewController {
-    
-    //MARK: Properties
-    private let myCarTableView = UITableView()
-    
-    //MARK: LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.systemBackground
-        
-        registerTableview()
+        self.view.backgroundColor = UIColor.white
     }
-    
-    //MARK: Method
-    func registerTableview() {
-        myCarTableView.delegate = self
-        myCarTableView.dataSource = self
-        myCarTableView.register(MyPageTableViewCell.self, forCellReuseIdentifier: MyPageTableViewCell.identifier)
-    }
-}
 
-extension MyPageViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: MyPageTableViewCell.identifier, for: indexPath) as? MyPageTableViewCell else { return UITableViewCell() }
-        return cell
-    }
-    
-    
-}
-
-// SwiftUI를 활용한 미리보기
-struct MyPageViewController_Previews: PreviewProvider {
-    static var previews: some View {
-        MyPageVCReprsentable().edgesIgnoringSafeArea(.all)
-    }
-}
-
-struct MyPageVCReprsentable: UIViewControllerRepresentable {
-    func makeUIViewController(context: Context) -> UIViewController {
-        let myPageVC = MyPageViewController()
-        return UINavigationController(rootViewController: myPageVC)
-    }
-    
-    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {}
-    typealias UIViewControllerType = UIViewController
 }
