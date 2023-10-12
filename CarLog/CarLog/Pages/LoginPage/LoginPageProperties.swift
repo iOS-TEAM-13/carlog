7a7de1b0804fc4e8f28b17b8f6e35647d503e5ec
Luna828 <eskw0701@naver.com>
2023-10-12 11:04:03 +0900

    [FEAT]: 회원가입 및 아이디 비밀번호 찾기 버튼 추가

--- CarLog/CarLog/Pages/LoginPage/LoginPageProperties.swift
+++ CarLog/CarLog/Pages/LoginPage/LoginPageProperties.swift
@@ -7,28 +7,22 @@ final class LoginPageProperties: UIView {
         return imageView
     }()
     
-    lazy var loginButton: UIButton = {
-        let button = UIButton()
-        button.customButton(text: "로 그 인", font: Constants.fontJua24 ?? UIFont(), titleColor: .primaryColor, backgroundColor: .white)
-        return button
-    }()
-    
     lazy var emailTextField: UITextField = {
         let textField = UITextField()
-        textField.loginCustomTextField(placeholder: "email", textColor: .lightGray, font: Constants.fontJua16 ?? UIFont(), alignment: .left, paddingView: UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.size.height)))
+        textField.loginCustomTextField(placeholder: "아이디 입력", textColor: .lightGray, font: Constants.fontJua16 ?? UIFont(), alignment: .left, paddingView: UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.size.height)))
         return textField
     }()
     
     lazy var passwordTextField: UITextField = {
         let textField = UITextField()
-        textField.loginCustomTextField(placeholder: "password", textColor: .lightGray, font: Constants.fontJua16 ?? UIFont(), alignment: .left, paddingView: UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.size.height)))
+        textField.loginCustomTextField(placeholder: "비밀번호 입력", textColor: .lightGray, font: Constants.fontJua16 ?? UIFont(), alignment: .left, paddingView: UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.size.height)))
         return textField
     }()
     
     lazy var checkboxButton: UIButton = {
         let button = UIButton()
-        button.widthAnchor.constraint(equalToConstant: 20).isActive = true
-        button.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        button.widthAnchor.constraint(equalToConstant: Constants.horizontalMargin).isActive = true
+        button.heightAnchor.constraint(equalToConstant: Constants.horizontalMargin).isActive = true
         button.layer.cornerRadius = 5
         button.backgroundColor = .white
         return button
@@ -42,7 +36,36 @@ final class LoginPageProperties: UIView {
     
     lazy var loginStatusStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [checkboxButton, loginStatusLabel])
-        stackView.customStackView(spacing: 10, axis: .horizontal, alignment: .center)
+        stackView.customStackView(spacing: Constants.verticalMargin, axis: .horizontal, alignment: .center)
+        return stackView
+    }()
+    
+    lazy var loginButton: UIButton = {
+        let button = UIButton()
+        button.customButton(text: "로 그 인", font: Constants.fontJua24 ?? UIFont(), titleColor: .primaryColor, backgroundColor: .white)
+        return button
+    }()
+    
+    lazy var signupButton: UIButton = {
+        let button = UIButton()
+        button.customButton(text: "회원가입", font: Constants.fontJua16 ?? UIFont(), titleColor: .white, backgroundColor: .clear)
+        return button
+    }()
+    
+    lazy var spaceView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    lazy var findIdPassword: UIButton = {
+        let button = UIButton()
+        button.customButton(text: "아이디 · 비밀번호 찾기", font: Constants.fontJua16 ?? UIFont(), titleColor: .white, backgroundColor: .clear)
+        return button
+    }()
+    
+    lazy var signupStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [signupButton, spaceView, findIdPassword])
+        stackView.customStackView(spacing: 0, axis: .horizontal, alignment: .fill)
         return stackView
     }()
     
@@ -81,68 +104,76 @@ final class LoginPageProperties: UIView {
     }()
     
     func setupUI() {
-           let safeArea = self.safeAreaLayoutGuide
-           addSubview(logo)
-           addSubview(emailTextField)
-           addSubview(passwordTextField)
-           addSubview(loginStatusStackView)
-           addSubview(loginButton)
-           addSubview(socialLoginDesignStackView)
-           addSubview(appleLoginButton)
+        let safeArea = safeAreaLayoutGuide
+        addSubview(logo)
+        addSubview(emailTextField)
+        addSubview(passwordTextField)
+        addSubview(loginStatusStackView)
+        addSubview(loginButton)
+        addSubview(signupStackView)
+        addSubview(socialLoginDesignStackView)
+        addSubview(appleLoginButton)
 
-           logo.snp.makeConstraints { make in
-               make.top.equalTo(safeArea.snp.top).offset(10)
-               make.leading.equalTo(safeArea.snp.leading).offset(85)
-               make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
-           }
+        logo.snp.makeConstraints { make in
+            make.top.equalTo(safeArea.snp.top).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(85)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
+        }
 
-           emailTextField.snp.makeConstraints { make in
-               make.top.equalTo(logo.snp.bottom).offset(10)
-               make.leading.equalTo(safeArea.snp.leading).offset(10)
-               make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
-           }
+        emailTextField.snp.makeConstraints { make in
+            make.top.equalTo(logo.snp.bottom).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(10)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
+        }
 
-           passwordTextField.snp.makeConstraints { make in
-               make.top.equalTo(emailTextField.snp.bottom).offset(10)
-               make.leading.equalTo(safeArea.snp.leading).offset(10)
-               make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
-           }
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(emailTextField.snp.bottom).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(10)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
+        }
 
-           loginStatusStackView.snp.makeConstraints { make in
-               make.top.equalTo(passwordTextField.snp.bottom).offset(15)
-               make.leading.equalTo(safeArea.snp.leading).offset(15)
-           }
+        loginStatusStackView.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(15)
+            make.leading.equalTo(safeArea.snp.leading).offset(15)
+        }
 
-           loginButton.snp.makeConstraints { make in
-               make.top.equalTo(passwordTextField.snp.bottom).offset(60)
-               make.leading.equalTo(safeArea.snp.leading).offset(10)
-               make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
-               make.width.equalTo(100)
-               make.height.equalTo(50)
-           }
+        loginButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordTextField.snp.bottom).offset(60)
+            make.leading.equalTo(safeArea.snp.leading).offset(10)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+        
+        signupStackView.snp.makeConstraints { make in
+            make.top.equalTo(loginButton.snp.bottom).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(15)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-15)
+        }
 
-           socialLoginDesignStackView.snp.makeConstraints { make in
-               make.top.equalTo(loginButton.snp.bottom).offset(10)
-               make.leading.equalTo(safeArea.snp.leading).offset(10)
-               make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
-               make.width.equalTo(100)
-               make.height.equalTo(50)
-           }
+        socialLoginDesignStackView.snp.makeConstraints { make in
+            make.top.equalTo(signupStackView.snp.bottom).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(10)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
 
-           appleLoginButton.snp.makeConstraints { make in
-               make.top.equalTo(socialLoginDesignStackView.snp.bottom).offset(10)
-               make.leading.equalTo(safeArea.snp.leading).offset(10)
-               make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
-               make.width.equalTo(100)
-               make.height.equalTo(50)
-           }
-       }
+        appleLoginButton.snp.makeConstraints { make in
+            make.top.equalTo(socialLoginDesignStackView.snp.bottom).offset(10)
+            make.leading.equalTo(safeArea.snp.leading).offset(10)
+            make.trailing.equalTo(safeArea.snp.trailing).offset(-10)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+    }
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupUI()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
