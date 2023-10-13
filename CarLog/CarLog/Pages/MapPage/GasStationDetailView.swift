147ba4a45eff8ba0abbdfea52f883689fe6a5ba4
user2rum <nauu400@naver.com>
2023-10-13 20:41:39 +0900

    [FEAT]: annotation 클릭 시 상세페이지 view 구현

---
+++ CarLog/CarLog/Pages/MapPage/GasStationDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  GasStationDetailView.swift
+//  CarLog
+//
+//  Created by t2023-m0075 on 10/13/23.
+//
+
+import UIKit
+
+class GasStationDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
