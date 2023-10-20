4169efecebe6d2069c0ef3b6e677b5c8844ae683
김은경 <luna828@gim-eungyeong-ui-MacBookPro.local>
2023-10-20 14:15:31 +0900

    [FEAT]: 회원탈퇴 다시 구현

--- CarLog/CarLog/Pages/CommunityPage/CommunityPageViewController.swift
+++ CarLog/CarLog/Pages/CommunityPage/CommunityPageViewController.swift
@@ -1,8 +1,7 @@
-import UIKit
 import Firebase
+import UIKit
 
 class CommunityPageViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor.white
@@ -13,7 +12,7 @@ class CommunityPageViewController: UIViewController {
         logoutButton.setTitleColor(.black, for: .normal)
         logoutButton.backgroundColor = .red
         logoutButton.layer.cornerRadius = 10
-        logoutButton.addTarget(self, action: #selector(logoutButtonTapped), for: .touchUpInside)
+        logoutButton.addTarget(self, action: #selector(self.logoutButtonTapped), for: .touchUpInside)
 
         // 버튼을 뷰 중앙에 추가
         self.view.addSubview(logoutButton)
@@ -23,16 +22,14 @@ class CommunityPageViewController: UIViewController {
     }
 
     @objc func logoutButtonTapped() {
-//        LoginService.loginService.logout {
-//            self.dismiss(animated: true)
-//        }
-//        LoginService.loginService.quitUser { user in
-//            if user != nil {
-//                self.dismiss(animated: true)
-//            }
-//    }
-        
-        LoginService.loginService.quitUser()
-        self.dismiss(animated: true)
+        if let email = Auth.auth().currentUser?.email {
+            LoginService.loginService.quitUser(email: email) { error in
+                if error == nil {
+                    self.dismiss(animated: true)
+                } else {
+                    print("회원탈퇴 실패 또는 오류: \(error?.localizedDescription ?? "알 수 없는 오류")")
+                }
+            }
+        }
     }
 }
