b80ee9907a458f1434c164222bd20641a999f4e5
gunnie <gunnieee91@gmail.com>
2023-10-20 21:12:35 +0900

    [FEAT]:MyPage UI 수정

--- CarLog/CarLog/Extension/UITextField+.swift
+++ CarLog/CarLog/Extension/UITextField+.swift
@@ -23,16 +23,19 @@ extension UITextField {
         self.leftViewMode = .always
     }
     
-    func mypageCustomTextField(placeholder: String, textColor: UIColor, font: UIFont, alignment: NSTextAlignment, paddingView: UIView){
+    func mypageCustomTextField(placeholder: String, textColor: UIColor, font: UIFont, alignment: NSTextAlignment){
+        let border = CALayer()
+        let width = CGFloat(1)
+        border.borderColor = UIColor.systemGray.cgColor
+        border.frame = CGRect(x: 0, y: 45, width: UIScreen.main.bounds.width - 40, height: 1)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
         self.placeholder = placeholder
         self.font = font
         self.sizeToFit()
         self.textAlignment = alignment
         self.textColor = textColor
         self.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        self.layer.cornerRadius = 15
-        self.backgroundColor = .thirdColor
-        self.leftView = paddingView
         self.leftViewMode = .always
     }
     
@@ -48,4 +51,6 @@ extension UITextField {
         self.rightView = paddingView
         self.rightViewMode = .always
     }
+     
+
 }
