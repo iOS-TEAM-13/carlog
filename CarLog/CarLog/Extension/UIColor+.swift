14592c113309b1c3ba1546e38358c82249f8a363
김은경 <luna828@gim-eungyeong-ui-MacBookPro.local>
2023-10-23 12:30:00 +0900

    [FEAT]: UIColor 변경

--- CarLog/CarLog/Extension/UIColor+.swift
+++ CarLog/CarLog/Extension/UIColor+.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 extension UIColor {
-    static let primaryColor = UIColor(red: 233/255, green: 239/255, blue: 255/255, alpha: 1.0)
-    static let firstColor = UIColor(red: 104/255, green: 127/255, blue: 246/255, alpha: 1.0)
-    static let thirdColor = UIColor(red: 0.08, green: 0.13, blue: 0.37, alpha: 0.8) //#15205E
+    static let mainNavyColor = UIColor(red: 0.08, green: 0.13, blue: 0.37, alpha: 0.8)
+    static let buttonSkyBlueColor = UIColor(red: 0.91, green: 0.94, blue: 1.00, alpha: 1.00)
 }
