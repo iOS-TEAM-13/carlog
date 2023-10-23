435d44b82a9afa833816e97777aff8de2806e100
김은경 <luna828@gim-eungyeong-ui-MacBookPro.local>
2023-10-23 10:05:01 +0900

    [FEAT]: UIColor 변경 (primary, third Color)

--- CarLog/CarLog/Extension/UIColor+.swift
+++ CarLog/CarLog/Extension/UIColor+.swift
@@ -1,12 +1,7 @@
 import UIKit
 
 extension UIColor {
-    static let primaryColor = UIColor(red: 39/255, green: 79/255, blue: 245/255, alpha: 1.0) //#274FF5
-    //static let primaryColor = UIColor(red: 11/255, green: 106/255, blue: 247/255, alpha: 1.0) //#0B6AF7
-    //static let primaryColor = UIColor(red: 73/255, green: 146/255, blue: 255/255, alpha: 1.0) //#4992FF
-    //static let primaryColor = UIColor(red: 0/255, green: 133/255, blue: 255/255, alpha: 1.0) //#0085FF
-    //static let primaryColor = UIColor(red: 50/255, green: 131/255, blue: 251/255, alpha: 1.0) //#3283FB
+    static let primaryColor = UIColor(red: 233/255, green: 239/255, blue: 255/255, alpha: 1.0)
     static let firstColor = UIColor(red: 104/255, green: 127/255, blue: 246/255, alpha: 1.0)
-    //static let secondColor = UIColor(red: 246/255, green: 183/255, blue: 87/255, alpha: 1.0)
-    static let thirdColor = UIColor(red: 233/255, green: 239/255, blue: 255/255, alpha: 1.0)
+    static let thirdColor = UIColor(red: 0.08, green: 0.13, blue: 0.37, alpha: 0.8) //#15205E
 }
