7b74168e9e407f37c0d2a6f585f4a22f97920a08
김은경 <luna828@gim-eungyeong-ui-MacBookPro.local>
2023-10-23 15:40:29 +0900

    [FEAT]: LoginService에 firestrore 연결하는 것 가져오기

--- CarLog/CarLog/Service/LoginService.swift
+++ CarLog/CarLog/Service/LoginService.swift
@@ -8,20 +8,16 @@ final class LoginService {
     static let loginService = LoginService()
     let db = Firestore.firestore()
 
-    func signUpUser(email: String, password: String ) {
+    func signUpUser(email: String, password: String) {
         Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
             if let error = error {
                 print("회원가입 실패: \(error.localizedDescription)")
                 return
             }
+
             if let email = authResult?.user.email {
-                let db = Firestore.firestore()
-                db.collection("users").addDocument(data: [
-                    "email": email
-                ]) { error in
-                    if let error = error {
-                        print("사용자 데이터 Firestore에 저장 실패: \(error.localizedDescription)")
-                    }
+                FirestoreService.firestoreService.saveUsers(user: User(email: email, password: password)) { err in
+                    print("err: \(err)")
                 }
             }
         }
