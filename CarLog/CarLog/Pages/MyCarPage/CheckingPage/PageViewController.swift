d675c3efff118868e1f66aeb5a332c71de8d385b
p2glet <vviian.2@gmail.com>
2023-10-24 11:50:35 +0900

    [FIX] tire <-> tireRotation

--- CarLog/CarLog/Pages/MyCarPage/CheckingPage/PageViewController.swift
+++ CarLog/CarLog/Pages/MyCarPage/CheckingPage/PageViewController.swift
@@ -21,7 +21,7 @@ class PageViewController: UIViewController {
     
     init(view: CheckingView, checkingView: componentsType) {
         super.init(nibName: nil, bundle: nil)
-        if checkingView == .engineOil || checkingView == .missionOil || checkingView == .brakeOil || checkingView == .brakePad || checkingView == .tire || checkingView == .tireRotation || checkingView == .fuelFilter || checkingView == .wiperBlade || checkingView == .airconFilter {
+        if checkingView == .engineOil || checkingView == .missionOil || checkingView == .brakeOil || checkingView == .brakePad || checkingView == .tireRotation || checkingView == .tire || checkingView == .fuelFilter || checkingView == .wiperBlade || checkingView == .airconFilter {
             componetsView = ComponentsView(view: view)
             type = componetsView?.checkTitleLabel.text ?? ""
             setupComponetsView()
