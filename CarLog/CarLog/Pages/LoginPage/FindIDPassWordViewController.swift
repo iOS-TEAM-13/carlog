90b1f784a0af2fadb34cd3a3381cc2aaa09c4d9a
Luna828 <lunakim0828@gmail.com>
2023-11-01 00:55:59 +0900

    [DESIGN]: 비밀번호 재설정 + 아이디 찾기 segment따라 UIview 나누기

--- CarLog/CarLog/Pages/LoginPage/FindIDPassWordViewController.swift
+++ CarLog/CarLog/Pages/LoginPage/FindIDPassWordViewController.swift
@@ -1,10 +1,3 @@
-//
-//  FindIDPassWordViewController.swift
-//  CarLog
-//
-//  Created by 김은경 on 10/31/23.
-//
-
 import UIKit
 
 import SnapKit
@@ -15,13 +8,14 @@ class FindIDPassWordViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        
         setupUI()
+        addTarget() // Call addTarget to set up the action for the segmented control.
     }
     
     private func setupUI() {
         view.addSubview(findIDPassWordView.segmentedControl)
-        view.addSubview(findIDPassWordView.emailTextField)
+        view.addSubview(findIDPassWordView.findIDView)
+        view.addSubview(findIDPassWordView.reSettingPasswordView)
         
         findIDPassWordView.segmentedControl.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide)
@@ -30,22 +24,26 @@ class FindIDPassWordViewController: UIViewController {
             make.height.equalTo(60)
         }
         
-        findIDPassWordView.emailTextField.snp.makeConstraints { make in
-            make.top.equalTo(findIDPassWordView.segmentedControl.snp.bottom).offset(Constants.verticalMargin * 2)
-            make.leading.equalTo(view.safeAreaLayoutGuide).offset(Constants.horizontalMargin)
-            make.trailing.equalTo(view.safeAreaLayoutGuide).offset(-Constants.horizontalMargin)
+        findIDPassWordView.findIDView.snp.makeConstraints { make in
+            make.top.equalTo(findIDPassWordView.segmentedControl.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide)
+            make.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
         }
         
+        findIDPassWordView.reSettingPasswordView.snp.makeConstraints { make in
+            make.top.equalTo(findIDPassWordView.segmentedControl.snp.bottom).offset(20)
+            make.leading.equalTo(view.safeAreaLayoutGuide)
+            make.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-10)
+        }
     }
     
     private func addTarget() {
         findIDPassWordView.segmentedControl.addTarget(self, action: #selector(didChangeValue(segment:)), for: .valueChanged)
     }
     
-    
-
     @objc private func didChangeValue(segment: UISegmentedControl) {
         findIDPassWordView.shouldHideFirstView = segment.selectedSegmentIndex != 0
     }
-
 }
--- CarLog/CarLog/Pages/LoginPage/FindIDPasswordView.swift
+++ CarLog/CarLog/Pages/LoginPage/FindIDPasswordView.swift
@@ -6,30 +6,38 @@
 //
 
 import UIKit
-
 import SnapKit
 
 class FindIDPasswordView: UIView {
-
     lazy var segmentedControl: UISegmentedControl = {
         let segmentedControl = UISegmentedControl(items: ["아이디 찾기", "비밀번호 재설정"])
         segmentedControl.selectedSegmentIndex = 0
-
         segmentedControl.selectedSegmentTintColor = .mainNavyColor
 
-        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.spoqaHanSansNeo(size: Constants.fontJua20, weight: .bold), .foregroundColor: UIColor.darkGray], for: .normal)
-        segmentedControl.setTitleTextAttributes([NSAttributedString.Key.font: UIFont.spoqaHanSansNeo(size: Constants.fontJua20, weight: .bold), .foregroundColor: UIColor.white], for: .selected)
+        segmentedControl.setTitleTextAttributes([.font: UIFont.spoqaHanSansNeo(size: Constants.fontJua20, weight: .bold), .foregroundColor: UIColor.darkGray], for: .normal)
+        segmentedControl.setTitleTextAttributes([.font: UIFont.spoqaHanSansNeo(size: Constants.fontJua20, weight: .bold), .foregroundColor: UIColor.white], for: .selected)
         return segmentedControl
     }()
-    
-    lazy var emailTextField: UITextField = {
-        let textField = UITextField()
-        textField.loginCustomTextField(placeholder: "이메일 입력", textColor: .black, font: UIFont.spoqaHanSansNeo(size: Constants.fontJua16, weight: .medium), alignment: .left, paddingView: UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.size.height)))
-        return textField
+
+    lazy var findIDView: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = .red
+        return uiView
     }()
-    
-    //lazy var findButton:
-    
+
+    lazy var reSettingPasswordView: UIView = {
+        let uiView = UIView()
+        uiView.backgroundColor = .mainNavyColor
+        uiView.isHidden = true
+        return uiView
+    }()
+
+//    lazy var emailTextField: UITextField = {
+//        let textField = UITextField()
+//        textField.loginCustomTextField(placeholder: "이메일 입력", textColor: .black, font: UIFont.spoqaHanSansNeo(size: Constants.fontJua16, weight: .medium), alignment: .left, paddingView: UIView(frame: CGRect(x: 0, y: 0, width: 15, height: textField.frame.size.height))
+//        return textField
+//    }()
+
     lazy var popButton: UIButton = {
         let button = UIButton()
         button.customButton(text: "취 소", font: UIFont.spoqaHanSansNeo(size: Constants.fontJua24, weight: .bold), titleColor: .buttonSkyBlueColor, backgroundColor: .mainNavyColor)
@@ -39,8 +47,9 @@ class FindIDPasswordView: UIView {
     var shouldHideFirstView: Bool? {
         didSet {
             guard let shouldHideFirstView = shouldHideFirstView else { return }
-//            self.drivingCollectionView.isHidden = shouldHideFirstView
-//            self.fuelingCollectionView.isHidden = !self.drivingCollectionView.isHidden
+            self.findIDView.isHidden = shouldHideFirstView
+            self.reSettingPasswordView.isHidden = !shouldHideFirstView
         }
     }
 }
+
