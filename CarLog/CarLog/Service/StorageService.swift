c2287c6b85588f4d49cb6c07d7a4f5586cd54145
Luna828 <lunakim0828@gmail.com>
2023-11-02 19:51:22 +0900

    [FEAT]: firebase 업로드시 url 받아오기 + download 기능 구현

--- CarLog/CarLog/Service/StorageService.swift
+++ CarLog/CarLog/Service/StorageService.swift
@@ -11,38 +11,38 @@ import FirebaseStorage
 class StorageService {
     static let storageService = StorageService()
 
-    func uploadImage2(image: UIImage) {
-        var imageData = Data()
+    func uploadImage(image: UIImage, pathRoot: String, completion: @escaping (URL?) -> Void) {
         guard let imageData = image.jpegData(compressionQuality: 0.4) else { return }
-        let filePath = "community"
         let metaData = StorageMetadata()
         metaData.contentType = "image/jpeg"
 
-        Storage.storage().reference().child(filePath).putData(imageData, metadata: metaData){ metaData, error in
+        let imageName = UUID().uuidString + String(Date().timeIntervalSince1970)
+
+        let firebaseReference = Storage.storage().reference().child("\(imageName)")
+        firebaseReference.putData(imageData, metadata: metaData) { _, error in
             if let error = error {
                 print(error.localizedDescription)
                 return
             } else {
                 print("성공")
-            }
-        }
-    }
-    
-    func uploadImage(image: UIImage, pathRoot: String, completion: @escaping (URL?) -> Void) {
-            guard let imageData = image.jpegData(compressionQuality: 0.4) else { return }
-            let metaData = StorageMetadata()
-            metaData.contentType = "image/jpeg"
-            
-            let imageName = UUID().uuidString + String(Date().timeIntervalSince1970)
-            
-            let firebaseReference = Storage.storage().reference().child("\(imageName)")
-            firebaseReference.putData(imageData, metadata: metaData) { metaData, error in
-                if let error = error {
-                    print(error.localizedDescription)
-                    return
-                } else {
-                    print("성공")
+                firebaseReference.downloadURL { url, _ in
+                    print(url)
+                    completion(url)
                 }
             }
         }
+    }
+
+    func downloadImage(urlString: String, completion: @escaping (UIImage?) -> Void) {
+        let storageReference = Storage.storage().reference(forURL: urlString)
+        let megaByte = Int64(1 * 1024 * 1024)
+
+        storageReference.getData(maxSize: megaByte) { data, _ in
+            guard let imageData = data else {
+                completion(nil)
+                return
+            }
+            completion(UIImage(data: imageData))
+        }
+    }
 }
