5af711df3707fa2360ee407f064b8e2a57f4fd74
Luna828 <lunakim0828@gmail.com>
2023-11-02 15:53:19 +0900

    [FEAT]: 파이어베이스 스토리지 로직 추가

--- CarLog/CarLog/Service/StorageService.swift
+++ CarLog/CarLog/Service/StorageService.swift
@@ -4,5 +4,27 @@
 //
 //  Created by 김은경 on 11/2/23.
 //
+import UIKit
 
-import Foundation
+import FirebaseStorage
+
+class StorageService {
+    static let storageService = StorageService()
+    
+    func uploadImage(image: UIImage, pathRoot: String, completion: @escaping (URL?) -> Void) {
+        guard let imageData = image.jpegData(compressionQuality: 0.4) else { return }
+        let metaData = StorageMetadata()
+        metaData.contentType = "image/jpeg"
+        
+        let imageName = UUID().uuidString + String(Date().timeIntervalSince1970)
+        
+        let firebaseReference = Storage.storage().reference().child("\(imageName)")
+        firebaseReference.putData(imageData, metadata: metaData) { metaData, error in
+            firebaseReference.downloadURL { url, _ in
+                completion(url)
+            }
+        }
+    }
+    
+    
+}
