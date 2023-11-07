a3b666dd92e38c51477e1285aa28f638e1cee380
luttoli <hhn0212@naver.com>
2023-11-07 16:06:27 +0900

    [FEAT]: 숫자 콤마찍기 컨포넌트 추가

--- CarLog/CarLog/Extension/UILabel+.swift
+++ CarLog/CarLog/Extension/UILabel+.swift
@@ -30,4 +30,10 @@ extension UILabel {
         return literUnitLabel
     }
     
+    //숫자 세자리마다 콤마찍기
+    func numberFormatter(number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        return numberFormatter.string(from: NSNumber(value: number))!
+    }
 }
