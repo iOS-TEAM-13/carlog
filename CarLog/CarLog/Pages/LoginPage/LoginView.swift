52447ed517db74e90234b85a8c5851d482e373dc
Luna828 <lunakim0828@gmail.com>
2023-11-14 14:05:03 +0900

    [FIX]: 로그인 버튼 공통 컴포넌트로 분리성공

--- CarLog/CarLog/Pages/LoginPage/LoginView.swift
+++ CarLog/CarLog/Pages/LoginPage/LoginView.swift
@@ -15,7 +15,8 @@ final class LoginView: UIView {
     lazy var emailTextField = loginTextField(placeholder: "이메일")
     lazy var passwordTextField = loginTextField(placeholder: "비밀번호")
 
-    lazy var loginButton = largeButton(text: "로 그 인", titleColor: .gray, backgroundColor: .lightGray)
+    //lazy var loginButton = Util.util.largeButton(text: "로 그 인", titleColor: .gray, backgroundColor: .lightGray)
+    let loginButton = LargeButtonView(title: "로 그 인")
     lazy var joinupButton = loginButton(text: "회원가입", font: UIFont.spoqaHanSansNeo(size: Constants.fontJua16, weight: .medium), titleColor: .black, backgroundColor: .clear)
     lazy var spaceView = UIView()
     lazy var findIdPassword = loginButton(text: "아이디 · 비밀번호 찾기", font: UIFont.spoqaHanSansNeo(size: Constants.fontJua16, weight: .medium), titleColor: .black, backgroundColor: .clear)
---
+++ CarLog/CarLog/Shared/LargeButtonView.swift
@@ -0,0 +1,58 @@
+//
+//  LargeButtonView.swift
+//  CarLog
+//
+//  Created by 김은경 on 11/14/23.
+//
+
+import SnapKit
+import UIKit
+
+class LargeButtonView: UIView {
+//    weak var delegate: ButtonTappedViewDelegate?
+
+    lazy var anyButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.setTitleColor(.gray, for: .selected)
+        button.isSelected = true
+        button.titleLabel?.font = UIFont.spoqaHanSansNeo(size: Constants.fontJua24, weight: .bold)
+        return button
+    }()
+
+    init(title: String) {
+        super.init(frame: CGRect.zero)
+        anyButton.setTitle(title, for: .normal)
+        setupUI()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension LargeButtonView {
+    func setupUI() {
+        setupButton()
+    }
+
+    func setupButton() {
+        addSubview(anyButton)
+        anyButton.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(50)
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+//extension LargeButtonView {
+//    @objc func buttonTapped(_ button: UIButton) {
+//        delegate?.didTapButton(button: button)
+//    }
+//}
+
